bf7424bdfda52b46fdb22c51653a7650514630e7
jose changir <changir@hotmail.com>
2024-08-08 20:48:13 -0400

    adicion ejercicio_01.py

---
+++ retos_sesion_04/ejercicio_01.py
@@ -0,0 +1,7 @@
+'''crear un archivo 'ejercicio_01.py', anade un comentatio con la descripcion
+del ejercicio '1' y crea un cuadro con prints'''
+
+print('****')
+print('*  *')
+print('*  *')
+print('****')
